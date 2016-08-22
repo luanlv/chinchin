be6520d8d50fb7abe867a3e97b5083e26719886c
Luan <mrvluan@gmail.com>
2016-08-22 19:00:17 +0700

    update

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shopchinchin.com/admin/');
+define('HTTP_CATALOG', 'http://shopchinchin.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shopchinchin.com/admin/');
+define('HTTPS_CATALOG', 'http://shopchinchin.com/');
+
+// DIR
+define('DIR_APPLICATION', '/usr/share/nginx/html/chinchin/admin/');
+define('DIR_SYSTEM', '/usr/share/nginx/html/chinchin/system/');
+define('DIR_LANGUAGE', '/usr/share/nginx/html/chinchin/admin/language/');
+define('DIR_TEMPLATE', '/usr/share/nginx/html/chinchin/admin/view/template/');
+define('DIR_CONFIG', '/usr/share/nginx/html/chinchin/system/config/');
+define('DIR_IMAGE', '/usr/share/nginx/html/chinchin/image/');
+define('DIR_CACHE', '/usr/share/nginx/html/chinchin/system/storage/cache/');
+define('DIR_DOWNLOAD', '/usr/share/nginx/html/chinchin/system/storage/download/');
+define('DIR_LOGS', '/usr/share/nginx/html/chinchin/system/storage/logs/');
+define('DIR_MODIFICATION', '/usr/share/nginx/html/chinchin/system/storage/modification/');
+define('DIR_UPLOAD', '/usr/share/nginx/html/chinchin/system/storage/upload/');
+define('DIR_CATALOG', '/usr/share/nginx/html/chinchin/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shopchinchin.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shopchinchin.com/');
+
+// DIR
+define('DIR_APPLICATION', '/usr/share/nginx/html/chinchin/catalog/');
+define('DIR_SYSTEM', '/usr/share/nginx/html/chinchin/system/');
+define('DIR_LANGUAGE', '/usr/share/nginx/html/chinchin/catalog/language/');
+define('DIR_TEMPLATE', '/usr/share/nginx/html/chinchin/catalog/view/theme/');
+define('DIR_CONFIG', '/usr/share/nginx/html/chinchin/system/config/');
+define('DIR_IMAGE', '/usr/share/nginx/html/chinchin/image/');
+define('DIR_CACHE', '/usr/share/nginx/html/chinchin/system/storage/cache/');
+define('DIR_DOWNLOAD', '/usr/share/nginx/html/chinchin/system/storage/download/');
+define('DIR_LOGS', '/usr/share/nginx/html/chinchin/system/storage/logs/');
+define('DIR_MODIFICATION', '/usr/share/nginx/html/chinchin/system/storage/modification/');
+define('DIR_UPLOAD', '/usr/share/nginx/html/chinchin/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
